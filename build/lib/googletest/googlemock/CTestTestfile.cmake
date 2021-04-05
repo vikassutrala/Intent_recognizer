6fe90374578d9ed57b9c855c28020a30012f3d71
vikas sutrala <vs-183455>
2021-04-05 18:10:31 +0200

    submodule for googletest has been added

--- build/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vikas/Intent_recognize/lib/googletest/googlemock
-# Build directory: /home/vikas/Intent_recognize/build/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/src/CMakeFiles/Intent_Recognizer_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libIntent_Recognizer_lib.a"
-)
